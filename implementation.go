3cf7d2c6edfffd854adca8777cb24e3998583d73
Anton <ponochovnyi.anton@LLL.kpi.ua>
2024-03-20 15:51:40 +0200

    add implementation.go

---
+++ implementation.go
@@ -0,0 +1,9 @@
+package Lab_2
+
+import "fmt"
+
+// TODO: document this function.
+// PrefixToPostfix converts
+func PrefixToPostfix(input string) (string, error) {
+	return "TODO", fmt.Errorf("TODO")
+}
