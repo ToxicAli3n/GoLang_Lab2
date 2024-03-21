d71ee798c5314a4c5441773889cf055713c52440
Anton <ponochovnyi.anton@LLL.kpi.ua>
2024-03-21 19:58:35 +0200

    add handler.go

---
+++ handler.go
@@ -0,0 +1,31 @@
+package Lab_2
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type ComputeHandler struct {
+	Input  io.Reader
+	Output io.Writer
+}
+
+func (ch *ComputeHandler) Compute() error {
+	reader := bufio.NewReader(ch.Input)
+	str, err := reader.ReadString('\n')
+	str = strings.TrimRight(str, "\n")
+	result, err := PrefixToPostfix(str)
+
+	if err != nil {
+		fmt.Fprintln(ch.Output, err)
+		return nil
+	}
+	_, err = ch.Output.Write([]byte(result))
+
+	if err != nil {
+		return fmt.Errorf("failed to write output: %v", err)
+	}
+	return nil
+}
